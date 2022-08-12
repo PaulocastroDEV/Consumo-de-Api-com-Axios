5905f2e86398b9ed46e10a76915b8e29a3603c14
PaulocastroDEV <pauloestudando159@gmail.com>
2022-08-12 14:32:58 -0300

    Deletando dados

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ app.post('/game',(req,res)=>{
     }
 }) 
 
+app.delete('/game/:id',(req,res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+        var index= DB.games.findIndex(g=> g.id==id);
+        if(index == -1){
+            res.sendStatus(404);
+        }else{
+            DB.games.splice(index,1);
+            res.sendStatus(200);
+        }
+    }
+})
 
 
 app.listen(3000, ()=>{
