b92258ef5287cbc9b8b1c197845d384bb9fd111f
PaulocastroDEV <pauloestudando159@gmail.com>
2022-08-12 14:37:51 -0300

    Edição de Dados

--- index.js
+++ index.js
@@ -64,15 +64,49 @@ app.post('/game',(req,res)=>{
 
 app.delete('/game/:id',(req,res)=>{
     if(isNaN(req.params.id)){
+
         res.sendStatus(400);
+
     }else{
         var id = parseInt(req.params.id);
+
         var index= DB.games.findIndex(g=> g.id==id);
+
         if(index == -1){
             res.sendStatus(404);
+
         }else{
             DB.games.splice(index,1);
             res.sendStatus(200);
+        }
+    }
+})
+app.put('/game/:id',(req,res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+
+        var game= DB.games.find(g=> g.id==id);
+
+        if(game != undefined){
+
+            var {title,price,year} = req.body;
+
+           if(title != undefined){
+            game.title = title;
+           }
+           if(price != undefined){
+            game.price = price;
+           }
+           if(year !=undefined){
+            game.year = year;
+           }
+
+
+            res.status(200).json(game)
+        }else{
+            res.sendStatus(404);
         }
     }
 })
