41c0592034acc9173302abc0a2975f6b098c6435
Paulo Gustavo <paulogustavo.dev@gmail.com>
2024-10-20 10:17:10 -0300

    fix: variables and node_modules

--- consumo de api/index.js
+++ consumo de api/index.js
@@ -1,99 +1,109 @@
-axios.get("http://localhost:3000/games").then(response =>{
-    let games = response.data;
-    let list = document.getElementById("games");
-    games.forEach(game =>{
-        let item = document.createElement("li");
+axios
+  .get("http://localhost:3000/games")
+  .then((response) => {
+    const games = response.data;
+    const list = document.getElementById("games");
+    games.forEach((game) => {
+      const item = document.createElement("li");
 
-        item.setAttribute("data-id",game.id);
-        item.setAttribute("data-title",game.title);
-        item.setAttribute("data-price",game.price);
-        item.setAttribute("data-year",game.year)
+      item.setAttribute("data-id", game.id);
+      item.setAttribute("data-title", game.title);
+      item.setAttribute("data-price", game.price);
+      item.setAttribute("data-year", game.year);
 
+      item.innerHTML =
+        "ID: " + game.id + " - " + game.title + " - $ " + game.price;
 
-        item.innerHTML = "ID: " + game.id + " - " + game.title + " - $ " + game.price;
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerHTML = "Deletar";
+      item.appendChild(deleteBtn);
+      deleteBtn.addEventListener("click", function () {
+        deleteGame(item);
+      });
 
-        let deleteBtn = document.createElement("button");
-        deleteBtn.innerHTML="Deletar";
-        item.appendChild(deleteBtn);
-        deleteBtn.addEventListener("click",function(){
-            deleteGame(item)
-        })
+      const editeBtn = document.createElement("button");
+      editeBtn.innerHTML = "Editar";
+      item.appendChild(editeBtn);
+      editeBtn.addEventListener("click", function () {
+        loadForm(item);
+      });
 
-        let editeBtn= document.createElement("button")
-        editeBtn.innerHTML="Editar";
-        item.appendChild(editeBtn);
-        editeBtn.addEventListener("click",function(){
-            loadForm(item);
-        })
+      list.appendChild(item);
+    });
+    console.log(games);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-        list.appendChild(item);
+function createGame() {
+  const titleInput = document.getElementById("title");
+  const yearInput = document.getElementById("year");
+  const priceInput = document.getElementById("price");
+
+  const game = {
+    title: titleInput.value,
+    year: yearInput.value,
+    price: priceInput.value,
+  };
+  axios
+    .post("http://localhost:3000/game", game)
+    .then((response) => {
+      if (response.status == 200) {
+        alert("Game cadastrado");
+      }
     })
-    console.log(games)
-}).catch(error =>{
-    console.log(error)
-});
-
-function createGame(){
-    let titleInput = document.getElementById("title");
-    let yearInput = document.getElementById("year");
-    let priceInput = document.getElementById("price");
-
-    let game={
-        title: titleInput.value,
-        year: yearInput.value,
-        price: priceInput.value
-    }
-    axios.post("http://localhost:3000/game",game).then(response=>{
-        if(response.status ==200){
-            alert("Game cadastrado");
-        }
-    }).catch(err=>{
-        console.log(err)
-    })
-    
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-function deleteGame(ListItem){
-    let id = ListItem.getAttribute("data-id");
-    axios.delete("http://localhost:3000/game/"+id).then(response=>{
-        if(response.status ==200){
-            alert("Game deltado");
-        }
-    }).catch(err=>{
-        console.log(err)
+function deleteGame(ListItem) {
+  const id = ListItem.getAttribute("data-id");
+  axios
+    .delete("http://localhost:3000/game/" + id)
+    .then((response) => {
+      if (response.status == 200) {
+        alert("Game deltado");
+      }
     })
+    .catch((err) => {
+      console.log(err);
+    });
 }
-function loadForm(ListItem){
-    let id=ListItem.getAttribute("data-id");
-    let title= ListItem.getAttribute("data-title");
-    let year = ListItem.getAttribute("data-year");
-    let price = ListItem.getAttribute("data-price"); 
+function loadForm(ListItem) {
+  const id = ListItem.getAttribute("data-id");
+  const title = ListItem.getAttribute("data-title");
+  const year = ListItem.getAttribute("data-year");
+  const price = ListItem.getAttribute("data-price");
 
-    document.getElementById("idEdit").value=id;
-    document.getElementById("titleEdit").value=title;
-    document.getElementById("yearEdit").value=year;
-    document.getElementById("priceEdit").value=price;
+  document.getElementById("idEdit").value = id;
+  document.getElementById("titleEdit").value = title;
+  document.getElementById("yearEdit").value = year;
+  document.getElementById("priceEdit").value = price;
 }
-function updateGame(){
-    var idInput = document.getElementById("idEdit")
-    let titleInput = document.getElementById("titleEdit");
-    let yearInput = document.getElementById("yearEdit");
-    let priceInput = document.getElementById("priceEdit");
+function updateGame() {
+  const idInput = document.getElementById("idEdit");
+  const titleInput = document.getElementById("titleEdit");
+  const yearInput = document.getElementById("yearEdit");
+  const priceInput = document.getElementById("priceEdit");
 
-    let game={
-        title: titleInput.value,
-        year: yearInput.value,
-        price: priceInput.value
-    }
+  const game = {
+    title: titleInput.value,
+    year: yearInput.value,
+    price: priceInput.value,
+  };
 
-    let id = idInput.value;
+  const id = idInput.value;
 
-    axios.put("http://localhost:3000/game/"+id,game).then(response=>{
-        if(response.status ==200){
-            alert("Game Atualizado");
-        }
-    }).catch(err=>{
-        console.log(err)
+  axios
+    .put("http://localhost:3000/game/" + id, game)
+    .then((response) => {
+      if (response.status == 200) {
+        alert("Game Atualizado");
+      }
     })
-    
+    .catch((err) => {
+      console.log(err);
+    });
 }
--- index.js
+++ index.js
@@ -1,119 +1,116 @@
 const express = require("express");
-const app= express();
-const bodyParser= require("body-parser");
-const cors= require('cors')
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //banco de dados falso
 
-var DB={
-    games: [
-        {
-            id:23,
-            title:"Call of duty MW",
-            year:2019,
-            price:60
-        },
-        {
-            id:65,
-            title:"Sea of thieves",
-            year:2018,
-            price:40
-        },
-        {
-            id:2,
-            title: "Minecraft",
-            year:2012,
-            price:20
-        }
-    ]
-}
-app.get('/games',(req, res)=>{
-    res.status(200).json(DB.games)
-})
+const db = {
+  games: [
+    {
+      id: 1,
+      title: "Call of duty MW",
+      year: 2019,
+      price: 60,
+    },
+    {
+      id: 2,
+      title: "Sea of thieves",
+      year: 2018,
+      price: 40,
+    },
+    {
+      id: 3,
+      title: "Minecraft",
+      year: 2012,
+      price: 20,
+    },
+  ],
+};
 
-app.get('/game/:id',(req,res)=>{
-    if(isNaN(req.params.id)){
-        res.sendStatus(400);
-    }else{
-        var id = parseInt(req.params.id);
-        var game= DB.games.find(g=> g.id==id);
-        if(game != undefined){
-            res.status(200).json(game)
-        }else{
-            res.sendStatus(404);
-        }
+let idGenerate = db.games[db.games.length - 1].id;
+
+app.get("/games", (req, res) => {
+  res.status(200).json(db.games);
+});
+
+app.get("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    const id = parseInt(req.params.id);
+    const game = db.games.find((g) => g.id == id);
+    if (game !== undefined) {
+      res.status(200).json(game);
+    } else {
+      res.sendStatus(404);
     }
-})
-app.post('/game',(req,res)=>{
-    var {title,price,year} = req.body;
-    if(isNaN(price) || isNaN(year) || typeof title != "string"){
-        res.sendStatus(400);
-    }else{
-        var game = req.body;
-        DB.games.push({
-            id:2325,
-            title,
-            price,
-            year
-        })
-        res.status(200).json(game)
+  }
+});
+app.post("/game", (req, res) => {
+  const { title, price, year } = req.body;
+  if (isNaN(price) || isNaN(year) || typeof title != "string") {
+    res.sendStatus(400);
+  } else {
+    const game = req.body;
+    db.games.push({
+      id: ++idGenerate,
+      title,
+      price,
+      year,
+    });
+    res.status(200).json(game);
+  }
+});
+
+app.delete("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    const id = parseInt(req.params.id);
+
+    const index = db.games.findIndex((g) => g.id == id);
+
+    if (index == -1) {
+      res.sendStatus(404);
+    } else {
+      db.games.splice(index, 1);
+      res.sendStatus(200);
     }
-}) 
+  }
+});
+app.put("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    const id = parseInt(req.params.id);
 
-app.delete('/game/:id',(req,res)=>{
-    if(isNaN(req.params.id)){
+    const game = db.games.find((g) => g.id == id);
 
-        res.sendStatus(400);
+    if (game !== undefined) {
+      const { title, price, year } = req.body;
 
-    }else{
-        var id = parseInt(req.params.id);
+      if (title !== undefined) {
+        game.title = title;
+      }
+      if (price !== undefined) {
+        game.price = price;
+      }
+      if (year !== undefined) {
+        game.year = year;
+      }
 
-        var index= DB.games.findIndex(g=> g.id==id);
-
-        if(index == -1){
-            res.sendStatus(404);
-
-        }else{
-            DB.games.splice(index,1);
-            res.sendStatus(200);
-        }
+      res.status(200).json(game);
+    } else {
+      res.sendStatus(404);
     }
-})
-app.put('/game/:id',(req,res)=>{
-    if(isNaN(req.params.id)){
-        res.sendStatus(400);
-    }else{
-        var id = parseInt(req.params.id);
+  }
+});
 
-        var game= DB.games.find(g=> g.id==id);
-
-        if(game != undefined){
-
-            var {title,price,year} = req.body;
-
-           if(title != undefined){
-            game.title = title;
-           }
-           if(price != undefined){
-            game.price = price;
-           }
-           if(year !=undefined){
-            game.year = year;
-           }
-
-
-            res.status(200).json(game)
-        }else{
-            res.sendStatus(404);
-        }
-    }
-})
-
-
-app.listen(3000, ()=>{
-    console.log("Api rodando");
-})
+app.listen(3000, () => {
+  console.log("Api rodando");
+});
